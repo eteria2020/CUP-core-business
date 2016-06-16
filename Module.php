78839a2189bcb5b40481655c9cbdbd4c34f1449a
Andrea Cadorin <cadorin89@gmail.com>
2016-06-16 10:04:26 +0200

    Clean Module.php

--- Module.php
+++ Module.php
@@ -41,15 +41,4 @@ class Module
     {
         return include __DIR__ . '/config/module.config.php';
     }
-
-    public function getAutoloaderConfig()
-    {
-        return [
-            'Zend\Loader\StandardAutoloader' => [
-                'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ],
-            ],
-        ];
-    }
 }
