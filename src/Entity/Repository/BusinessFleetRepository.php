ac7606e2b9afaaf07d8b53df50fa289c594c90a7
Enrico Taddei <enrico.taddei@sharengo.eu>
2018-01-26 18:19:40 +0100

    fleet-180126-dummy-fleet: upadte 18:19

--- src/Entity/Repository/BusinessFleetRepository.php
+++ src/Entity/Repository/BusinessFleetRepository.php
@@ -3,10 +3,28 @@
 namespace BusinessCore\Entity\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use BusinessCore\Entity\BusinessFleet;
 
 /**
  * BusinessFleetRepository
  */
-class BusinessFleetRepository extends EntityRepository
-{
+class BusinessFleetRepository extends EntityRepository {
+
+    /**
+     *
+     * Return all BusinessFleet not dummy (id < 100)
+     * @return BusinessFleet[]
+     */
+    public function getFeetNoDummy() {
+        $em = $this->getEntityManager();
+
+        $dql = "SELECT f
+        FROM \BusinessCore\Entity\BusinessFleet f
+        WHERE f.id < " . BusinessFleet::DUMMY_FLEET_LIMIT;
+
+        $query = $em->createQuery($dql);
+
+        return $query->getResult();
+    }
+
 }
